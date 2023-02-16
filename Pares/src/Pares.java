81efa668740414741e7e268c049d0a83afe4c91d
abrmoralesmart <abrmoralesmart@gmail.com>
2023-02-16 16:10:50 +0100

    añadido ejercicio de pares

---
+++ Pares/src/Pares.java
@@ -0,0 +1,12 @@
+
+public class Pares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 100; i >= 0; i-=2){
+				//if ( i% 2 == 0) 
+			System.out.println(i);
+		}
+	}
+
+}
